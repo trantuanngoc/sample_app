e076baba91543a7c3e657ca67d5870f46e492963
fancol <ngocthoson1@gmail.com>
2020-08-05 16:42:19 +0700

    Add full titel helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #return title for each page
+    def full_title(page_title='')
+        base_title="Sample app"
+        if page_title.empty?
+            base_title
+        else
+            page_title
+        end
+    end  
 end
