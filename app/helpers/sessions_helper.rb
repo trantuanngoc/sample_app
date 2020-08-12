77307cf4583f57f7a6e052f0d7029caa4b382dcb
fancol <ngocthoson1@gmail.com>
2020-08-12 14:09:54 +0700

    Update indent

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,15 +14,15 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
-    if (user_id = session[:user_id])
-        @current_user ||= User.find_by(id: user_id)
-      elsif (user_id = cookies.encrypted[:user_id])
-        user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
-          log_in user
-          @current_user = user
-        end
+    if(user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
       end
+    end
   end
 
   # Returns true if the user is logged in, false otherwise.
