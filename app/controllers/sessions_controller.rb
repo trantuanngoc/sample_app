45a70715bf671dd42be647bf03f3b35e3cf52100
fancol <ngocthoson1@gmail.com>
2020-08-12 14:09:54 +0700

    Update indent

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user=User.find_by(email: params[:session][:email].downcase)
-    if user&&user.authenticate(params[:session][:password])
+    user= User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       reset_session
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,23 +11,23 @@ module SessionsHelper
     cookies.permanent.encrypted[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
+
   # Returns the current logged-in user (if any).
   def current_user
-    if (user_id = session[:user_id])
-        @current_user ||= User.find_by(id: user_id)
-      elsif (user_id = cookies.encrypted[:user_id])
-        user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
-          log_in user
-          @current_user = user
-        end
+    if(user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
       end
+    end
   end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
-    !current_user.nil?
+    current_user
   end
 
   # Forgets a persistent session.
