56d7fc91d49a645ddfd77756692c2a2211b69436
fancol <ngocthoson1@gmail.com>
2020-08-14 17:26:00 +0700

    Temporary

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_08_14_080333) do
-=======
-ActiveRecord::Schema.define(version: 2020_08_11_092341) do
->>>>>>> Implement advanced login
-=======
-ActiveRecord::Schema.define(version: 2020_08_14_080333) do
->>>>>>> Finish updating users
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,14 +19,7 @@ ActiveRecord::Schema.define(version: 2020_08_14_080333) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
-<<<<<<< HEAD
-<<<<<<< HEAD
     t.boolean "admin"
-=======
->>>>>>> Implement advanced login
-=======
-    t.boolean "admin"
->>>>>>> Finish updating users
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
