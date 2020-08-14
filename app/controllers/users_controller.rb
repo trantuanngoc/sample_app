542f8e48a36dcbda3b207cf3d32c768d42a51886
fancol <ngocthoson1@gmail.com>
2020-08-14 18:04:08 +0700

    Fix indent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user=User.new
+    @user = User.new
   end
 
   def show
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
