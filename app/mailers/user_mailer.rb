7f8ca3233889a3206496378c379cd43b834958d4
fancol <ngocthoson1@gmail.com>
2020-08-17 11:55:42 +0700

    Fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,6 @@ class UserMailer < ApplicationMailer
 
   def password_reset
     @greeting = "Hi"
-
     mail to: "to@example.org"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ User.create!(name:  "Example User",
 
 # Generate a bunch of additional users.
 99.times do |n|
-name  = Faker::Name.name
+name = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
 User.create!(name: name, email: email,
