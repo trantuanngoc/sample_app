034dea2ec09a5dbd302185baadcb9101597d67d1
fancol <ngocthoson1@gmail.com>
2020-08-19 10:05:24 +0700

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,53 +1,61 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-    attr_accessor :remember_token, :activation_token, :reset_token
-    before_save   :downcase_email
-    before_create :create_activation_digest
-    validates :name, presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },uniqueness: true
-    has_secure_password :password, validations: false
-    validates :password, presence: true, length: {minimum: 6}
+  has_many :active_relationships, class_name:  "Relationship",
+    foreign_key: "follower_id",
+    dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+    foreign_key: "followed_id",
+    dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },uniqueness: true
+  has_secure_password :password, validations: false
+  validates :password, presence: true, length: {minimum: 6}
 
-    # Returns the hash digest of the given string.
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-    end
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+  end
 
-    # Returns a random token.
-    def User.new_token
-        SecureRandom.urlsafe_base64
-    end
+  # Returns a random token.
+  def User.new_token
+      SecureRandom.urlsafe_base64
+  end
 
-    # Remembers a user in the database for use in persistent sessions.
-    def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
-    end
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
-    # Returns true if the given token matches the digest.
-    def authenticated?(attribute, token)
-      digest = send("#{attribute}_digest")
-      return false if digest.nil?
-      BCrypt::Password.new(digest).is_password?(token)
-    end
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
-    # Forgets a user.
-    def forget
-        update_attribute(:remember_digest, nil)
-    end
+  # Forgets a user.
+  def forget
+      update_attribute(:remember_digest, nil)
+  end
 
-    # Activates an account.
-    def activate
-      update_attribute(:activated, true)
-      update_attribute(:activated_at, Time.zone.now)
-    end
+  # Activates an account.
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
 
-    # Sends activation email.
-    def send_activation_email
-      UserMailer.account_activation(self).deliver_now
-    end
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
     # Sets the password reset attributes.
   def create_reset_digest
@@ -67,7 +75,24 @@ class User < ApplicationRecord
   end
 
   def feed
-    self.microposts.all
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
